fc36eaa8d8d8b2aa369f379b86a6f405145b5831
sktpgns <sog3283@naver.com>
2024-11-07 10:41:25 +0900

    v2.5 BP2

--- week10/week10_dataset.py
+++ week10/week10_dataset.py
@@ -1,9 +1,18 @@
 import seaborn as sns
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import title
 
-print(sns.get_dataset_names())
-#tutanic = sns.load_dataset('titanic')
-mpg = sns.load_dataset('mpg')
-print(mpg.head())
+titanic = sns.load_dataset('titanic')
+#print(titanic['embarked'], titanic['deck'], titanic['class'])
+#print(titanic['deck'])
+#titanic['deck'] = titanic['deck'].cat.add_categories('Unknown')
+#print(titanic['deck'])
 
+print(titanic['survived'])
 
+sns.countplot(data=titanic, x="survived")
+plt,title(('survived (0 = No,1 = Yes)'))
+plt.xlabel('survived')
+plt.ylabel('count')
+plt.show()
