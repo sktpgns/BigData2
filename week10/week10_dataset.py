8fdc7533e10172a1c64b03b66a62c8b62c5590bd
sktpgns <sog3283@naver.com>
2024-11-07 11:12:01 +0900

    v2.6 BP2

--- week10/week10_dataset.py
+++ week10/week10_dataset.py
@@ -4,15 +4,29 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import title
 
 titanic = sns.load_dataset('titanic')
+
+
+
 #print(titanic['embarked'], titanic['deck'], titanic['class'])
 #print(titanic['deck'])
 #titanic['deck'] = titanic['deck'].cat.add_categories('Unknown')
 #print(titanic['deck'])
 
-print(titanic['survived'])
+#print(titanic['survived'])
 
-sns.countplot(data=titanic, x="survived")
-plt,title(('survived (0 = No,1 = Yes)'))
-plt.xlabel('survived')
-plt.ylabel('count')
+# sns.countplot(data=titanic, x="survived")
+# plt,title(('survived (0 = No,1 = Yes)'))
+# plt.xlabel('survived')
+# plt.ylabel('count')
+# plt.show()
+
+# gender_survval = titanic.groupby(by='sex')['survived'].mean()
+# print(type(gender_survval))
+gender_survval = titanic.groupby(by='sex')['survived'].mean().reset_index()
+print(type(gender_survval))
+
+sns.barplot(data=gender_survval, x = 'sex', y='survived')
+plt.title('survival rate by gender')
+plt.ylabel('survival rate')
 plt.show()
+
